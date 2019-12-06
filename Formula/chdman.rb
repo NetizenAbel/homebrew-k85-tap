3c3b307931e48649c2aad077f5fbfaf69290a283
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-06 14:06:14 -0800

    Update chdman.rb

--- Formula/chdman.rb
+++ Formula/chdman.rb
@@ -1,25 +1,15 @@
 class Chdman < Formula
   desc "MAME CHDMAN binary"
-  version "0.216"
   homepage "https://github.com/kashaiahyah85/chdman"
-  url "https://github.com/kashaiahyah85/chdman/releases/download/#{version}/chdman.zip"
+  url "https://github.com/kashaiahyah85/chdman/releases/download/0.126/chdman.zip"
   sha256 "797cc69e2f067a11ee99cc94ee59bbe1b1dd99f3c82fa8f2d3ab6e2b5d5fb475"
 
-  bottle do
-    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :catalina
-    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :mojave
-    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :high_sierra
-    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :sierra
-    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :el_capitan
-    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :yosemite
-  end
-
   def install
-    ENV.delete "SDKROOT" if MacOS.version == :yosemite
-    install, "chdman"
+    bin.install "chdman/chdman"
+    bin.env_script_all_files("/usr/bin")
   end
 
   test do
-    system "#{bin}/chdman", "--version"
+    system /usr/bin/chdman
   end
 end
