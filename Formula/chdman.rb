d15c2eff3a4ae60bc42ab2ba0a02c30d1ad81890
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-06 14:12:06 -0800

    Update chdman.rb

--- Formula/chdman.rb
+++ Formula/chdman.rb
@@ -1,10 +1,10 @@
 class Chdman < Formula
   desc "MAME CHDMAN binary"
   homepage "https://github.com/kashaiahyah85/chdman"
-  bottle :unneeded
   url "https://github.com/kashaiahyah85/chdman/releases/download/0.126/chdman.zip"
   sha256 "797cc69e2f067a11ee99cc94ee59bbe1b1dd99f3c82fa8f2d3ab6e2b5d5fb475"
-
+  bottle :unneeded
+  
   def install
     bin.install "chdman/chdman"
     bin.env_script_all_files("/usr/bin")
