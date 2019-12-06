a645719196ea811d01d7c166002820de6035d1ce
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-06 15:05:22 -0800

    Update chdman.rb

--- Formula/chdman.rb
+++ Formula/chdman.rb
@@ -4,7 +4,7 @@ class Chdman < Formula
   url "https://github.com/kashaiahyah85/chdman/releases/download/0.216/chdman.zip"
   sha256 "797cc69e2f067a11ee99cc94ee59bbe1b1dd99f3c82fa8f2d3ab6e2b5d5fb475"
   bottle :unneeded
-  depends_on "sdl2"
+  depends_on "sdl2", "sdl2_ttf"
   
   def install
     bin.install "chdman"
