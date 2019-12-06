3f0fe64c314cce4097e1a1f84a6be15f0dd4df32
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-06 15:45:58 -0800

    Update chdman.rb

--- Formula/chdman.rb
+++ Formula/chdman.rb
@@ -2,7 +2,7 @@ class Chdman < Formula
   desc "MAME CHDMAN binary"
   homepage "https://github.com/kashaiahyah85/chdman"
   url "https://github.com/kashaiahyah85/chdman/releases/download/0.216/chdman.zip"
-  sha256 "797cc69e2f067a11ee99cc94ee59bbe1b1dd99f3c82fa8f2d3ab6e2b5d5fb475"
+  sha256 "f94203c7a71518d4abd17546244a72d34ccdd3b46033595a32983961bfa70c87"
 
   bottle :unneeded
   depends_on "sdl2_ttf"
