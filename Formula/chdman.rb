47c82c38b4ce0f9cbfa85dfd5d157f3e1ccea561
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-06 13:33:56 -0800

    Create chdman.rb

---
+++ Formula/chdman.rb
@@ -0,0 +1,26 @@
+class Chdman < Formula
+  desc "MAME CHDMAN binary"
+  homepage "https://www.advancemame.it/"
+  url "https://github.com/kashaiahyah85/chdman/archive/chdman.zip"
+  sha256 "797cc69e2f067a11ee99cc94ee59bbe1b1dd99f3c82fa8f2d3ab6e2b5d5fb475"
+
+  bottle do
+    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :catalina
+    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :mojave
+    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :high_sierra
+    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :sierra
+    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :el_capitan
+    sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :yosemite
+  end
+
+  conflicts_with "mame-tools", :because => "both install `chdman` binaries"
+
+  def install
+    ENV.delete "SDKROOT" if MacOS.version == :yosemite
+    system "install", "chdman"
+  end
+
+  test do
+    system "#{bin}/chdman", "--version"
+  end
+end
