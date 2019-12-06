a9d1ab11c528915980f00c58c0ee2265604250dd
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-06 13:56:07 -0800

    Update chdman.rb

--- Formula/chdman.rb
+++ Formula/chdman.rb
@@ -1,7 +1,7 @@
 class Chdman < Formula
   desc "MAME CHDMAN binary"
   version "0.216"
-  homepage "https://www.advancemame.it/"
+  homepage "https://github.com/kashaiahyah85/chdman"
   url "https://github.com/kashaiahyah85/chdman/releases/download/#{version}/chdman.zip"
   sha256 "797cc69e2f067a11ee99cc94ee59bbe1b1dd99f3c82fa8f2d3ab6e2b5d5fb475"
 
@@ -14,11 +14,9 @@ class Chdman < Formula
     sha256 "af16e65db7bde341438c61b6f41c3b346b152a22aaca4f0d4292f6c8b71c7874" => :yosemite
   end
 
-  conflicts_with "mame-tools", :because => "both install `chdman` binaries"
-
   def install
     ENV.delete "SDKROOT" if MacOS.version == :yosemite
-    system "install", "chdman"
+    install, "chdman"
   end
 
   test do
